1e9ed711abe3f0209a8d9852feb4421e1621518d
austin <austinjdiehl@gmail.com>
2022-03-22 19:34:42 -0500

    Finished Assignment

--- queries.sql
+++ queries.sql
@@ -1,10 +1,18 @@
 -- Part 1: Test it with SQL
+CREATE TABLE job (
 id int,
 employer varchar(255),
 name varchar(255),
-skills varchar(255);
+skills varchar(255)
+);
 -- Part 2: Test it with SQL
-
+SELECT *
+FROM employer
+WHERE location="St. Louis City";
 -- Part 3: Test it with SQL
-
+DROP TABLE job;
 -- Part 4: Test it with SQL
+SELECT * FROM skill
+LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
